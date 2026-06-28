b3d6e6f5c722b16b00318d7648904e44b6349055
kmrahulmojahid543-creator <your-github-email@example.com>
2026-06-28 10:42:15 +0600

    Upload Laravel Ecommerce project with custom hero section

---
+++ resources/views/client/components/molecules/about/warranty.blade.php
@@ -0,0 +1,12 @@
+<div class="warranty py-md-5 py-2" id="warranty">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <h3 class="font-primary"><u>Warranty</u></h3>
+      </div>
+      <div class="col-lg-8 col-md-8 col-12">
+        <p class="font-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis omnis libero quisquam velit nemo nihil aperiam adipisci a sint explicabo aspernatur delectus ut tenetur, saepe harum, modi ex est nostrum! Temporibus dignissimos accusantium eaque impedit.</p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/client/components/molecules/hero/text-block.blade.php
@@ -0,0 +1,8 @@
+@push('css')
+    <link rel="stylesheet" href="{{ asset('client/components/molecules/hero/text-block.css') }}">
+@endpush
+
+<div class="text-center text-block-hero py-5">
+    <h1>Your Choice is your real Strength</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, tempore. Molestiae voluptatem reiciendis harum odio, earum quas incidunt veritatis alias vitae, dicta expedita.</p>
+</div>
